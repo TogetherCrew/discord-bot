d524cf4c51f30e03e272fa5c8e74746e0140afa9
zuies <zurabichkhikvadze66@gmail.com>
2023-05-10 16:18:27 +0400

    feat:install eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
-import { connectDB } from "./database";
-import config from "./config";
-import * as Sentry from "@sentry/node";
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { connectDB } from './database';
+import config from './config';
+import * as Sentry from '@sentry/node';
 
 Sentry.init({
   dsn: config.sentry.dsn,
@@ -9,7 +9,9 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
 
 client.once(Events.ClientReady, async (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
