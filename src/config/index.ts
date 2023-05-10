342a3bf38f92b651481a3a343aaab687acc93a74
zuies <zurabichkhikvadze66@gmail.com>
2023-05-10 16:38:02 +0400

    feat:add ci.yml for github actions

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,29 +1,29 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string()
-      .valid("production", "development", "local", "test")
+      .valid('production', 'development', 'local', 'test')
       .required(),
-    DB_HOST: Joi.string().required().description("Mongo DB url"),
-    DB_PORT: Joi.string().required().description("Mongo DB port"),
-    DB_USER: Joi.string().required().description("Mongo DB username"),
-    DB_PASSWORD: Joi.string().required().description("Mongo DB password"),
-    DB_NAME: Joi.string().required().description("Mongo DB name"),
-    DISCROD_CLIENT_ID: Joi.string().required().description("Discord clinet id"),
+    DB_HOST: Joi.string().required().description('Mongo DB url'),
+    DB_PORT: Joi.string().required().description('Mongo DB port'),
+    DB_USER: Joi.string().required().description('Mongo DB username'),
+    DB_PASSWORD: Joi.string().required().description('Mongo DB password'),
+    DB_NAME: Joi.string().required().description('Mongo DB name'),
+    DISCROD_CLIENT_ID: Joi.string().required().description('Discord clinet id'),
     DISCORD_CLIENT_SECRET: Joi.string()
       .required()
-      .description("Discord clinet secret"),
-    DISCORD_BOT_TOKEN: Joi.string().required().description("Discord bot token"),
-    SENTRY_DSN: Joi.string().required().description("Sentry DSN"),
+      .description('Discord clinet secret'),
+    DISCORD_BOT_TOKEN: Joi.string().required().description('Discord bot token'),
+    SENTRY_DSN: Joi.string().required().description('Sentry DSN'),
     SENTRY_ENV: Joi.string()
-      .valid("production", "development", "local", "test")
+      .valid('production', 'development', 'local', 'test')
       .required(),
   })
   .unknown();
 
 const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: "key" } })
+  .prefs({ errors: { label: 'key' } })
   .validate(process.env);
 
 if (error) {
@@ -33,11 +33,11 @@ export default {
   env: envVars.NODE_ENV,
   mongoose: {
     serverURL:
-      envVars.NODE_ENV === "local" || envVars.NODE_ENV === "test"
+      envVars.NODE_ENV === 'local' || envVars.NODE_ENV === 'test'
         ? `mongodb://127.0.0.1:27017/RnDAO-${envVars.NODE_ENV}`
         : `mongodb://${envVars.DB_USER}:${envVars.DB_PASSWORD}@${envVars.DB_HOST}:${envVars.DB_PORT}/${envVars.DB_NAME}`,
     botURL:
-      envVars.NODE_ENV === "local" || envVars.NODE_ENV === "test"
+      envVars.NODE_ENV === 'local' || envVars.NODE_ENV === 'test'
         ? `mongodb://127.0.0.1:27017`
         : `mongodb://${envVars.DB_USER}:${envVars.DB_PASSWORD}@${envVars.DB_HOST}:${envVars.DB_PORT}`,
   },
