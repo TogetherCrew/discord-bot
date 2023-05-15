b57d6f5d3acac1370866fc2540337ce21c02fac6
zuies <zurabichkhikvadze66@gmail.com>
2023-05-15 14:40:42 +0400

    feat:create rawInfo service and write unit tests

---
+++ __tests__/unit/database/services/rawInfo.test.ts
@@ -0,0 +1,202 @@
+import mongoose, { Connection } from 'mongoose';
+import { IRawInfo, rawInfoSchema } from 'tc_dbcomm';
+import setupTestDB from '../../../utils/setupTestDB';
+import {
+  rawInfo1,
+  rawInfo2,
+  rawInfo3,
+} from '../../../fixtures/rawInfo.fixture';
+import { rawInfoService } from '../../database/services';
+import config from '../../../../src/config';
+
+setupTestDB();
+
+describe('rawInfo service', () => {
+  let connection: Connection;
+
+  beforeAll(async () => {
+    connection = await mongoose.createConnection(config.mongoose.serverURL, {
+      dbName: 'connection',
+    });
+    connection.model<IRawInfo>('RawInfo', rawInfoSchema);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  beforeEach(async () => {
+    await connection.db.dropDatabase();
+  });
+
+  describe('createRawInfo', () => {
+    test('should create a rawInfo', async () => {
+      const result = await rawInfoService.createRawInfo(connection, rawInfo1);
+      expect(result).toBeDefined();
+      expect(result.messageId).toEqual(rawInfo1.messageId);
+    });
+  });
+
+  describe('createRawInfos', () => {
+    test('should create rawInfos (list of rawInfo)', async () => {
+      const result = await rawInfoService.createRawInfos(connection, [
+        rawInfo1,
+        rawInfo2,
+        rawInfo3,
+      ]);
+      expect(result).toMatchObject([rawInfo1, rawInfo2, rawInfo3]);
+    });
+  });
+
+  describe('getRawInfo', () => {
+    test('should retrieve an existing rawInfo that matches the filter criteria', async () => {
+      await rawInfoService.createRawInfo(connection, rawInfo3);
+      const result = await rawInfoService.getRawInfo(connection, {
+        messageId: rawInfo3.messageId,
+      });
+      expect(result).toMatchObject(rawInfo3);
+    });
+
+    test('should return null when no rawInfo matches the filter criteria', async () => {
+      await rawInfoService.createRawInfo(connection, rawInfo3);
+      const result = await rawInfoService.getRawInfo(connection, {
+        role_mentions: ['role8'],
+      });
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('getRawInfos', () => {
+    test('should retrieve rawInfo that matches the filter criteria', async () => {
+      await rawInfoService.createRawInfos(connection, [
+        rawInfo1,
+        rawInfo2,
+        rawInfo3,
+      ]);
+      const result = await rawInfoService.getRawInfos(connection, {
+        role_mentions: rawInfo2.role_mentions,
+      });
+      expect(result).toMatchObject([rawInfo1, rawInfo2]);
+    });
+
+    test('should return an empty array when no rawInfo matches the filter criteria', async () => {
+      await rawInfoService.createRawInfos(connection, [
+        rawInfo1,
+        rawInfo2,
+        rawInfo3,
+      ]);
+      const result = await rawInfoService.getRawInfos(connection, {
+        role_mentions: ['role8'],
+      });
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('updateRawInfo', () => {
+    const patchPayload: IRawInfo = {
+      channelId: 'channel1',
+      messageId: 'message123',
+      threadId: 'thread456',
+      content: 'new content',
+    };
+
+    test('should update an existing rawInfo that matches the filter criteria', async () => {
+      await rawInfoService.createRawInfo(connection, rawInfo1);
+      const result = await rawInfoService.updateRawInfo(
+        connection,
+
+        { messageId: rawInfo1.messageId },
+        patchPayload
+      );
+
+      expect(result).toMatchObject(patchPayload);
+    });
+
+    test('should return null when no rawInfo matches the filter criteria', async () => {
+      const result = await rawInfoService.updateRawInfo(
+        connection,
+        { messageId: rawInfo1.messageId },
+        patchPayload
+      );
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('updateRawInfos', () => {
+    const patchPayload: IRawInfo = {
+      channelId: 'channel1',
+      messageId: 'message123',
+      threadId: 'thread456',
+      content: 'new content',
+    };
+
+    test('should update rawInfos that match the filter criteria', async () => {
+      await rawInfoService.createRawInfos(connection, [
+        rawInfo1,
+        rawInfo2,
+        rawInfo3,
+      ]);
+      const result = await rawInfoService.updateRawInfos(
+        connection,
+        { content: rawInfo1.content },
+        patchPayload
+      );
+      expect(result).toEqual(2);
+      const rawInfo1Doc = await rawInfoService.getRawInfo(connection, {
+        content: rawInfo1.content,
+      });
+      const rawInfo2Doc = await rawInfoService.getRawInfo(connection, {
+        content: rawInfo2.content,
+      });
+      expect(rawInfo1Doc?.content).toBe(patchPayload.content);
+      expect(rawInfo2Doc?.content).toBe(patchPayload.content);
+    });
+
+    test('should return 0 when no rawInfos match the filter criteria', async () => {
+      const result = await rawInfoService.updateRawInfos(
+        connection,
+        { content: rawInfo3.content },
+        patchPayload
+      );
+      expect(result).toEqual(0);
+    });
+  });
+
+  describe('deleteRawInfo', () => {
+    test('should delete rawInfo that matches the filter criteria', async () => {
+      await rawInfoService.createRawInfo(connection, rawInfo1);
+      const result = await rawInfoService.deleteRawInfo(connection, {
+        content: rawInfo1.content,
+      });
+      expect(result).toEqual(true);
+    });
+
+    test('should return false when no rawInfo matches the filter criteria', async () => {
+      const result = await rawInfoService.deleteRawInfo(connection, {
+        content: rawInfo1.content,
+      });
+      expect(result).toEqual(false);
+    });
+  });
+
+  describe('deleteRawInfos', () => {
+    test('should delete rawInfo that matches the filter criteria', async () => {
+      await rawInfoService.createRawInfos(connection, [
+        rawInfo1,
+        rawInfo2,
+        rawInfo3,
+      ]);
+      const result = await rawInfoService.deleteRawInfos(connection, {
+        content: rawInfo1.content,
+      });
+      expect(result).toEqual(2);
+    });
+
+    test('should return 0 when no rawInfo matches the filter criteria', async () => {
+      const result = await rawInfoService.deleteRawInfos(connection, {
+        content: rawInfo1.content,
+      });
+      expect(result).toEqual(0);
+    });
+  });
+});
---
+++ src/database/services/rawInfo.service.ts
@@ -0,0 +1,169 @@
+import { Connection } from 'mongoose';
+import { IRawInfo } from 'tc_dbcomm';
+
+/**
+ * Create a rawInfo entry in the database.
+ * @param {Connection} connection - Mongoose connection object for the database.
+ * @param {IRawInfo} rawInfo - The rawInfo object to be created.
+ * @returns {Promise<IRawInfo>} - A promise that resolves to the created rawInfo object.
+ */
+async function createRawInfo(
+  connection: Connection,
+  rawInfo: IRawInfo
+): Promise<IRawInfo> {
+  try {
+    return await connection.models.RawInfo.create(rawInfo);
+  } catch (error) {
+    console.log(error);
+    throw new Error('Failed to create rawInfo');
+  }
+}
+
+/**
+ * Create multiple rawInfo entries in the database.
+ * @param {Connection} connection - Mongoose connection object for the database.
+ * @param {IRawInfo[]} rawInfos - An array of rawInfo objects to be created.
+ * @returns {Promise<IRawInfo[]>} - A promise that resolves to an array of the created rawInfo objects.
+ */
+async function createRawInfos(
+  connection: Connection,
+  rawInfos: IRawInfo[]
+): Promise<IRawInfo[]> {
+  try {
+    return await connection.models.RawInfo.insertMany(
+      rawInfos.map((rawInfo) => rawInfo)
+    );
+  } catch (error) {
+    throw new Error('Failed to create rawInfos');
+  }
+}
+
+/**
+ * Get a rawInfo entry from the database based on the filter criteria.
+ * @param {Connection} connection - Mongoose connection object for the database.
+ * @param {object} filter - An object specifying the filter criteria to match the desired rawInfo entry.
+ * @returns {Promise<IRawInfo | null>} - A promise that resolves to the matching rawInfo object or null if not found.
+ */
+async function getRawInfo(
+  connection: Connection,
+  filter: object
+): Promise<IRawInfo | null> {
+  try {
+    return await connection.models.RawInfo.findOne(filter);
+  } catch (error) {
+    throw new Error('Failed to retrieve rawInfo');
+  }
+}
+
+/**
+ * Get rawInfo entries from the database based on the filter criteria.
+ * @param {Connection} connection - Mongoose connection object for the database.
+ * @param {object} filter - An object specifying the filter criteria to match the desired rawInfo entries.
+ * @returns {Promise<IRawInfo[]>} - A promise that resolves to an array of the matching rawInfo objects.
+ */
+async function getRawInfos(
+  connection: Connection,
+  filter: object
+): Promise<IRawInfo[]> {
+  try {
+    return await connection.models.RawInfo.find(filter);
+  } catch (error) {
+    throw new Error('Failed to retrieve rawInfos');
+  }
+}
+
+/**
+ * Update a rawInfo entry in the database based on the filter criteria.
+ * @param {Connection} connection - Mongoose connection object for the database.
+ * @param {object} filter - An object specifying the filter criteria to match the desired rawInfo entry.
+ * @param {IRawInfo} UpdateBody - An object containing the updated rawInfo data.
+ * @returns {Promise<IRawInfo | null>} - A promise that resolves to the updated rawInfo object or null if not found.
+ */
+async function updateRawInfo(
+  connection: Connection,
+  filter: object,
+  UpdateBody: IRawInfo
+): Promise<IRawInfo | null> {
+  try {
+    const rawInfo = await connection.models.RawInfo.findOne(filter);
+    if (!rawInfo) {
+      return null;
+    }
+    Object.assign(rawInfo, UpdateBody);
+    return await rawInfo.save();
+  } catch (error) {
+    throw new Error('Failed to update rawInfo');
+  }
+}
+
+/**
+ * Update multiple rawInfo entries in the database based on the filter criteria.
+ * @param {Connection} connection - Mongoose connection object for the database.
+ * @param {object} filter - An object specifying the filter criteria to match multiple rawInfo entries.
+ * @param {IRawInfo} UpdateBody - An object containing the updated rawInfo data.
+ * @returns {Promise<number>} - A promise that resolves to the number of updated rawInfo entries.
+ */
+async function updateManyRawInfo(
+  connection: Connection,
+  filter: object,
+  UpdateBody: IRawInfo
+): Promise<number> {
+  try {
+    const updateResult = await connection.models.RawInfo.updateMany(
+      filter,
+      UpdateBody
+    );
+    return updateResult.modifiedCount || 0;
+  } catch (error) {
+    throw new Error('Failed to update rawInfos');
+  }
+}
+
+/**
+ * Delete a rawInfo entry from the database based on the filter criteria.
+ * @param {Connection} connection - Mongoose connection object for the database.
+ * @param {object} filter - An object specifying the filter criteria to match the desired rawInfo entry for deletion.
+ * @returns {Promise<boolean>} - A promise that resolves to true if the rawInfo entry was successfully deleted, or false otherwise.
+ * @throws {Error} - If there is an error while deleting the rawInfo entry.
+ */
+async function deleteRawInfo(
+  connection: Connection,
+  filter: object
+): Promise<boolean> {
+  try {
+    const deleteResult = await connection.models.RawInfo.deleteOne(filter);
+    return deleteResult.deletedCount === 1;
+  } catch (error) {
+    throw new Error('Failed to delete rawInfo');
+  }
+}
+
+/**
+ * Delete multiple rawInfo entries from the database based on the filter criteria.
+ * @param {Connection} connection - Mongoose connection object for the database.
+ * @param {object} filter - An object specifying the filter criteria to match multiple rawInfo entries for deletion.
+ * @returns {Promise<number>} - A promise that resolves to the number of deleted rawInfo entries.
+ * @throws {Error} - If there is an error while deleting the rawInfo entries.
+ */
+async function deleteManyRawInfo(
+  connection: Connection,
+  filter: object
+): Promise<number> {
+  try {
+    const deleteResult = await connection.models.RawInfo.deleteMany(filter);
+    return deleteResult.deletedCount;
+  } catch (error) {
+    throw new Error('Failed to delete rawInfos');
+  }
+}
+
+export default {
+  createRawInfo,
+  createRawInfos,
+  updateRawInfo,
+  updateManyRawInfo,
+  deleteRawInfo,
+  deleteManyRawInfo,
+  getRawInfo,
+  getRawInfos,
+};
