33c1f49c3104e02cdc72dd683d478d31c37263db
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-06-01 19:44:29 +0400

    [FIXBUGS]

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,11 +1,9 @@
 import { Events, Client } from 'discord.js';
-import { connectDB } from '../../database';
 
 export default {
   name: Events.ClientReady,
   once: true,
   execute(client: Client) {
     console.log(`Ready! Logged in as ${client.user?.tag}`);
-    connectDB();
   },
 };
