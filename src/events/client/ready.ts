145afc1dd67bf4a7f1902bc4c0b98ec70910958c
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-06-12 22:52:00 +0400

    [FEATURE]: get list of guild members for exiting guilds

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,9 +1,20 @@
 import { Events, Client } from 'discord.js';
+import { databaseService } from '@togethercrew.dev/db'
+import { guildService } from '../../database/services';
+import fetchMembers from '../../functions/fetchMembers';
+import config from '../../config';
 
 export default {
   name: Events.ClientReady,
   once: true,
-  execute(client: Client) {
-    console.log(`Ready! Logged in as ${client.user?.tag}`);
+  async execute(client: Client) {
+    console.log(`client READY event is running`)
+    const guilds = await guildService.getGuilds({ isDisconnected: false });
+    for (let i = 0; i < guilds.length; i++) {
+      console.log(`client READY event is running for ${guilds[i].guildId}:${guilds[i].name}`)
+      const connection = databaseService.connectionFactory(guilds[i].guildId, config.mongoose.dbURL);
+      await fetchMembers(connection, client, guilds[i].guildId)
+      console.log(`client READY event is Done ${guilds[i].guildId}:${guilds[i].name}`)
+    }
   },
 };
