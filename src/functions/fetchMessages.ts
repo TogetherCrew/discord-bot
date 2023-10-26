e2c5e3277da624594072e4aa3b0f91822f4579b9
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-10-26 16:11:16 +0330

    [FEATURE]: increase the JOB time limit + save 100 msgs

--- src/functions/fetchMessages.ts
+++ src/functions/fetchMessages.ts
@@ -152,27 +152,27 @@ async function fetchMessages(
         }
         channel instanceof ThreadChannel
           ? await pushMessagesToArray(connection, messagesToStore, [...fetchedMessages.values()], {
-              threadId: channel.id,
-              threadName: channel.name,
-              channelId: channel.parent?.id,
-              channelName: channel.parent?.name,
-            })
+            threadId: channel.id,
+            threadName: channel.name,
+            channelId: channel.parent?.id,
+            channelName: channel.parent?.name,
+          })
           : await pushMessagesToArray(connection, messagesToStore, [...fetchedMessages.values()]);
         break;
       }
 
       channel instanceof ThreadChannel
         ? await pushMessagesToArray(connection, messagesToStore, [...fetchedMessages.values()], {
-            threadId: channel.id,
-            threadName: channel.name,
-            channelId: channel.parent?.id,
-            channelName: channel.parent?.name,
-          })
+          threadId: channel.id,
+          threadName: channel.name,
+          channelId: channel.parent?.id,
+          channelName: channel.parent?.name,
+        })
         : await pushMessagesToArray(connection, messagesToStore, [...fetchedMessages.values()]);
+      await rawInfoService.createRawInfos(connection, messagesToStore);
       options[fetchDirection] = boundaryMessage.id;
       fetchedMessages = await channel.messages.fetch(options);
     }
-    await rawInfoService.createRawInfos(connection, messagesToStore);
   } catch (err) {
     logger.error(
       { guild_id: connection.name, channel_id: channel.id, fetchDirection, err },
