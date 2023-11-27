2ecf2b0d6feac65cbf0e5eee6f7c210ea5b3e7ec
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2023-11-27 16:46:52 +0330

    [FEATURE]: update migration to CC

--- src/migrations/db/1695210587863-add-isgeneratedbyweebhook-to-rawinfo-schema.ts
+++ src/migrations/db/1695210587863-add-isgeneratedbyweebhook-to-rawinfo-schema.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { Client, GatewayIntentBits, } from 'discord.js';
-import { guildService } from '../../database/services';
+import { platformService } from '../../database/services';
 import { connectDB } from '../../database';
 import config from '../../config';
 import webhookLogic from '../utils/webhookLogic';
@@ -22,10 +22,10 @@ export const up = async () => {
 
     await client.login(config.discord.botToken);
     await connectDB();
-    const guilds = await guildService.getGuilds({});
-    for (let i = 0; i < guilds.length; i++) {
-        const connection = DatabaseManager.getInstance().getTenantDb(guilds[i].guildId);
-        await webhookLogic(connection, client, guilds[i].guildId);
+    const platforms = await platformService.getPlatforms({});
+    for (let i = 0; i < platforms.length; i++) {
+        const connection = DatabaseManager.getInstance().getTenantDb(platforms[i].metadata?.id);
+        await webhookLogic(connection, client, platforms[i].metadata?.id);
     }
 };
 
