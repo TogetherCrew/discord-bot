b649573fcb93c097855243662fad8b19860fde08
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-01-18 18:14:19 +0330

    [DEBUG]

--- src/rabbitmq/events/interactionResponseCreate.ts
+++ src/rabbitmq/events/interactionResponseCreate.ts
@@ -7,12 +7,18 @@ const logger = parentLogger.child({ module: `${Event.DISCORD_BOT.INTERACTION_RES
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function handleInteractionResponseCreate(msg: any) {
     try {
-        logger.info({ msg, event: Event.DISCORD_BOT.INTERACTION_RESPONSE.CREATE }, 'is running');
+        logger.info({ msg, event: Event.DISCORD_BOT.INTERACTION_RESPONSE.CREATE, }, 'is running');
         const interaction: ChatInputCommandInteraction_broker = JSON.parse(msg?.content.interaction);
         const data: InteractionResponse = JSON.parse(msg?.content.data);
+
+        console.log(interaction)
+
+        console.log(data)
+
         await interactionService.createInteractionResponse(interaction, data);
         logger.info({ msg, event: Event.DISCORD_BOT.INTERACTION_RESPONSE.CREATE }, 'is done');
     } catch (error) {
+        console.log(error)
         logger.error({ msg, event: Event.DISCORD_BOT.INTERACTION_RESPONSE.CREATE, error }, 'is failed');
     }
 }
