ebaf19162937227c8cceaa994788d8b9bd23e00b
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-01-29 10:31:07 +0330

    [CI]: fixed lint errors

--- mongo-init.js
+++ mongo-init.js
@@ -1,4 +1,4 @@
 print('Started Adding the Users.');
 db = db.getSiblingDB('RnDAO');
-db.createUser({ user: 'root', pwd: 'pass', roles: ['readWrite'] });
+db.createUser({ user: 'user', pwd: 'password', roles: ['readWrite'] });
 print('End Adding the User Roles.');
