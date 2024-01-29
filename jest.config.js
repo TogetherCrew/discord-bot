6e98f3fdfc63d213623ec4cce30d8010ff7d3179
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-01-29 09:33:13 +0330

    [CI]: fixed lint errors

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,20 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    testPathIgnorePatterns: [
-        "/__tests__/fixtures",
-        "/__tests__/utils",
-    ],
-    transform: {
-        '^.+\\.(ts|tsx)?$': 'ts-jest',
-        "^.+\\.(js|jsx)$": "babel-jest",
+  testPathIgnorePatterns: ['/__tests__/fixtures', '/__tests__/utils'],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+  collectCoverage: true,
+  testEnvironment: 'node',
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'html'],
+  collectCoverageFrom: ['src/**/*.ts*'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
     },
-    collectCoverage: true,
-    testEnvironment: "node",
-    coverageReporters: ["json", "lcov", "text", "clover", "html"],
-    collectCoverageFrom: [
-        "src/**/*.ts*"
-    ],
-    coverageThreshold: {
-        global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: -10,
-        },
-    }
+  },
 };
--- mongo-init.js
+++ mongo-init.js
@@ -1,4 +1,4 @@
-print("Started Adding the Users.");
-db = db.getSiblingDB("RnDAO");
-db.createUser({ user: "user", pwd: "1234", roles: ["readWrite"] })
-print("End Adding the User Roles.");
+print('Started Adding the Users.');
+db = db.getSiblingDB('RnDAO');
+db.createUser({ user: 'root', pwd: 'pass', roles: ['readWrite'] });
+print('End Adding the User Roles.');
