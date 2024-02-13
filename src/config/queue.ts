f7a90efc20bbcfc60646ad07859193b2119cda48
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-02-13 18:05:40 +0330

    fix the cronJob time

--- src/config/queue.ts
+++ src/config/queue.ts
@@ -7,7 +7,7 @@ export const redisConfig = {
 };
 
 export const cronJobConfig = {
-  cron: '* * * * *', // Run once at 00:00 UTC
+  cron: '0 0 * * *', // Run once at 00:00 UTC
   jobId: 'cronJob',
   attempts: 0,
   backoff: {
