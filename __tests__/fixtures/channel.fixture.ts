549dcef8c0f93414fbfa9da9918e763466d6baa7
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-02-29 18:47:55 +0330

    fix tests

--- __tests__/fixtures/channel.fixture.ts
+++ __tests__/fixtures/channel.fixture.ts
@@ -19,18 +19,21 @@ export const channel1: IChannel = {
       deny: '',
     },
   ],
+  type: 0
 };
 
 export const channel2: IChannel = {
   channelId: '234567890123456789',
   name: 'Channel 2',
   parentId: '987654321098765432',
+  type: 0
 };
 
 export const channel3: IChannel = {
   channelId: '345678901234567890',
   name: 'Channel 3',
   parentId: '987654321098765432',
+  type: 0
 };
 
 export const insertChannels = async function <Type>(channels: Array<Type>, connection: Connection) {
