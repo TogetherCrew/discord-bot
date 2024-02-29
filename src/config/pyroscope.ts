6f5cd24ffd71d1343a317334b59441605e9a5936
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-02-29 17:09:20 +0330

    enable the pyroscope

--- src/config/pyroscope.ts
+++ src/config/pyroscope.ts
@@ -1,13 +1,13 @@
-// import Pyroscope from '@pyroscope/nodejs';
-// import config from './index';
-// import logger from './logger';
+import Pyroscope from '@pyroscope/nodejs';
+import config from './index';
+import logger from './logger';
 
-// export default function pyroscope(): void {
-//   try {
-//     Pyroscope.init({ serverAddress: config.pyroscope.address, appName: 'discord' });
-//     Pyroscope.start();
-//     logger.info({ url: config.pyroscope.address }, 'Connected to pyroscope.');
-//   } catch (error) {
-//     logger.fatal({ url: config.pyroscope.address, error }, 'Failed to connect to pyroscope.');
-//   }
-// }
+export default function pyroscope(): void {
+  try {
+    Pyroscope.init({ serverAddress: config.pyroscope.address, appName: 'discord' });
+    Pyroscope.start();
+    logger.info({ url: config.pyroscope.address }, 'Connected to pyroscope.');
+  } catch (error) {
+    logger.fatal({ url: config.pyroscope.address, error }, 'Failed to connect to pyroscope.');
+  }
+}
