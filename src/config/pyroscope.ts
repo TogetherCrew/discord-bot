bad403883e7bf74e1cc1efdf66f52c06fde10a03
Behzad Rabiei <53224485+Behzad-rabiei@users.noreply.github.com>
2024-05-15 21:51:39 +0400

    format the code

--- src/config/pyroscope.ts
+++ src/config/pyroscope.ts
@@ -3,11 +3,11 @@ import config from './index';
 import logger from './logger';
 
 export default function pyroscope(): void {
-    try {
-        Pyroscope.init({ serverAddress: config.pyroscope.address, appName: 'discord' });
-        Pyroscope.start();
-        logger.info({ url: config.pyroscope.address }, 'Connected to pyroscope.');
-    } catch (error) {
-        logger.fatal({ url: config.pyroscope.address, error }, 'Failed to connect to pyroscope.');
-    }
+  try {
+    Pyroscope.init({ serverAddress: config.pyroscope.address, appName: 'discord' });
+    Pyroscope.start();
+    logger.info({ url: config.pyroscope.address }, 'Connected to pyroscope.');
+  } catch (error) {
+    logger.fatal({ url: config.pyroscope.address, error }, 'Failed to connect to pyroscope.');
+  }
 }
