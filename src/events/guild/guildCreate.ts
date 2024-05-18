66c6c314f7ebfff7fdc7db7295547c880b8d458b
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2024-05-18 17:07:43 +0400

    add GuildCreate event

---
+++ src/events/guild/guildCreate.ts
@@ -0,0 +1,13 @@
+import { Events, type Guild } from 'discord.js';
+import { addGuildEventQueue } from '../../queue/queues/guildEvent';
+import { guildMemberService } from '../../database/services';
+import config from '../../config';
+export default {
+    name: Events.GuildCreate,
+    once: false,
+    async execute(guild: Guild) {
+        const botMember = await guild.members.fetch(config.discord.clientId);
+        const dataToStore = guildMemberService.getNeededDateFromGuildMember(botMember);
+        addGuildEventQueue({ type: Events.GuildMemberUpdate, guildId: botMember.guild.id, dataToStore });
+    },
+};
