3607e6c324a5f1f37a69cbfc6ceb147d87e178cd
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2024-05-25 17:38:02 +0400

    format the code

--- src/scripts/deleteRnDAOGuildCommands.ts
+++ src/scripts/deleteRnDAOGuildCommands.ts
@@ -8,23 +8,21 @@ const logger = parentLogger.child({ module: `deleteRnDAOGuildCommands` });
  * Delete RnDAO guild commands
  */
 async function deleteRnDAOGuildCommands(): Promise<void> {
-    try {
-        const rest = new REST().setToken(config.discord.botToken);
-        const guildCommands: any = await rest.get(
-            // RnDAO:915914985140531240
-            Routes.applicationGuildCommands(config.discord.clientId, '915914985140531240')
-        );
-        guildCommands.forEach(async (command: any) => {
-            await rest.delete(
-                Routes.applicationGuildCommand(config.discord.clientId, '915914985140531240', command.id)
-            );
-        });
-    } catch (error) {
-        console.log(error)
-        logger.error('Failed to delete RnDAO guild commands', error);
-    }
+  try {
+    const rest = new REST().setToken(config.discord.botToken);
+    const guildCommands: any = await rest.get(
+      // RnDAO:915914985140531240
+      Routes.applicationGuildCommands(config.discord.clientId, '915914985140531240'),
+    );
+    guildCommands.forEach(async (command: any) => {
+      await rest.delete(Routes.applicationGuildCommand(config.discord.clientId, '915914985140531240', command.id));
+    });
+  } catch (error) {
+    console.log(error);
+    logger.error('Failed to delete RnDAO guild commands', error);
+  }
 }
 
 deleteRnDAOGuildCommands().catch((error) => {
-    logger.error('Unhandled exception in deleting RnDAO guild commands', error);
+  logger.error('Unhandled exception in deleting RnDAO guild commands', error);
 });
