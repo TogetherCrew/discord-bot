f42c84339084ea1f85d2f3e32bb3469b8e317990
Behzad Rabiei <behzad.rabiei.77@gmail.com>
2024-10-27 19:18:31 +0400

    fix workflows

--- hivemind-adapter/src/main.ts
+++ hivemind-adapter/src/main.ts
@@ -5,6 +5,6 @@ import { Logger } from 'nestjs-pino'
 async function bootstrap() {
     const app = await NestFactory.createApplicationContext(AppModule)
     const logger = app.get(Logger)
-    logger.log('App is running....')
+    logger.log('App is running...')
 }
 bootstrap()
