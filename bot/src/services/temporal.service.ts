5c636f9c17ac6974a339601bd43a199cad17bac1
cyri113 <c.derche@me.com>
2025-03-29 12:04:50 +0100

    add TEMPORAL_URI to test env

--- bot/src/services/temporal.service.ts
+++ bot/src/services/temporal.service.ts
@@ -1,10 +1,9 @@
 import { Client, Connection } from '@temporalio/client'
 import config from '../config'
 
-
 export async function createTemporalClient(): Promise<Client> {
-  const connection = await Connection.connect({ address: config.temporal.uri })
-  return new Client({
-    connection,
-  })
+    const connection = await Connection.connect({ address: config.temporal.uri })
+    return new Client({
+        connection,
+    })
 }
